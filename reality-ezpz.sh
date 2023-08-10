62121669ccc0949ec89f7b7304bb82e29dcf3674
aleskxyz <39186039+aleskxyz@users.noreply.github.com>
2023-08-10 15:09:56 +0200

    Update sing-box

--- reality-ezpz.sh
+++ reality-ezpz.sh
@@ -39,7 +39,7 @@ WIDTH=60
 CHOICE_HEIGHT=20
 
 image[xray]="teddysun/xray:1.8.3"
-image[sing-box]="gzxhwq/sing-box:v1.3.5"
+image[sing-box]="gzxhwq/sing-box:v1.3.6"
 image[nginx]="nginx:1.24.0"
 image[certbot]="certbot/certbot:v2.6.0"
 image[haproxy]="haproxy:2.8.0"
